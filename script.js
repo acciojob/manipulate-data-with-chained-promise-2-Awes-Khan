c35c1803fed3a1145792357f23d4e45232373251
Awes Khan <44608315+Awes-Khan@users.noreply.github.com>
2025-01-09 14:57:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+const arr = [1,2,3,4];
+
+new Promise((resolve) =>{
+	setTimeout(()=>{
+		resolve(arr);
+	},3000);
+}).then((arr)=>{
+    return new Promise(resolve=>{
+  		setTimeout(()=>{
+  				resolve(arr.filter(a => a%2==0));
+  		},1000);
+  		
+  	});
+  }).then((result)=>{
+      return new Promise(resolve=>{
+    		setTimeout(()=>{
+    		  document.getElementById('output').innerHTML= result.map(num=>num*2).join(",");
+    		  resolve();
+    		},2000);
+  	});
+});
